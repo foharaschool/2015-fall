db5dcf207ed0f0c09576c8701c2c4a743ad4afeb
Fred O'Hara <fred.ohara@johnsonfit.com>
2015-11-04 07:31:44 -0600

    11/4 Backup

--- 2015IntroProg/Documents/unit4/jsFiles/lab12firstFunctionCall.js
+++ 2015IntroProg/Documents/unit4/jsFiles/lab12firstFunctionCall.js
@@ -1,3 +1,9 @@
+
+//The purpose of this function is to write a message for the user
+function callMessage() {
+    document.write("Hi, from the f<br />");
+}
+
 /*
     This is the JavaScript code for Part 1 of
     "Lab 1: Calling Functions in JavaScript" 
@@ -6,10 +12,10 @@
 function lab12firstFunctionCallPart1() {
     // PART 1: YOUR CODE STARTS AFTER THIS LINE:
     
-    // Delete the following statement when you start your code.
-    document.write("This is the function \"lab12firstFunctionCallPart1()\" " 
-            + "<br />and is in the JavaScript file: " 
-            + "\"/unit5/jsFiles/lab12firstFunctionCall.js\"");
+    // Call the message
+    for(var i = 0; i < 5; i++) {
+        callMessage();
+    }
 }
 
 
@@ -18,11 +24,19 @@ function lab12firstFunctionCallPart1() {
     "Lab 1: Calling Functions in JavaScript" 
     File: /unit5/labs/lab01firstFunctionCall.html
 */
+//
+function generateName() {
+    var firstName = prompt("enter your first name");
+    var middleName = prompt("enter your midde name");
+    var lastName = prompt("enter your last name");
+    
+    document.write("Hello " + firstName + " " + middleName + " " + lastName);
+}
+
 function lab12firstFunctionCallPart2() {
     // PART 2: YOUR CODE STARTS AFTER THIS LINE:
-    
-    // Delete the following statement when you start your code.
-    document.write("This is the function \"lab12firstFunctionCallPart2()\" " 
-            + "<br />and is in the JavaScript file: " 
-            + "\"/unit5/jsFiles/lab12firstFunctionCall.js\"");
+        
+    generateName();
+    document.write(". Shall we play a game?");
+
 }
