690b5c13956589aae5e037c235bffa4c760208a8
foharaschool <fohara@madisoncollege.edu>
2015-12-09 20:52:07 -0600

    Intro Prog Lab and Project5 start

--- 2015IntroProg/Documents/unit5/jsFiles/lab09parallelArrays.js
+++ 2015IntroProg/Documents/unit5/jsFiles/lab09parallelArrays.js
@@ -3,13 +3,45 @@
     "Lab 9: Parallel Arrays" 
     File: /unit5/labs/lab09parallelArrays.html
 */
+
+
+//Scores:
+ 
+    var scores = [76, 99, 56, 78, 72, 50, 54, 86, 75, 55, 61, 54, 81, 97, 93,
+            74, 51, 86, 67, 83, 80, 61, 97, 55, 75, 54, 92, 74, 52, 76,
+            73, 82, 73, 74, 88, 64, 66, 56, 95, 97, 68, 85, 79, 92, 67,
+            57, 88, 52, 93, 79];
+ 
+//Names:
+ 
+    var names = ["Latashia Peeks", "Boyd Girone", "Kimiko Lints",
+            "Arminda Renicker", "Emanuel Ingber", "Winston Nakama",
+            "Marlen Offerman", "Toby Huxford", "Courtney Schmidgall",
+            "Isidra Edgmon", "Randal Colver", "Theola Betancur",
+            "Timmy Outram", "Timmy Crossgrove", "Cary Goldammer",
+            "Isa Gamage", "Timmy Foulkes", "Elois Surgeon",
+            "Roman Eggett", "Shirleen Omary", "Abel Newfield",
+            "Armanda Matts", "Mirtha Bensing", "Shery Rauls", 
+            "Abel Wahpekeche", "Van Mazzie", "Magdalen Sopher",
+            "Jerald Nasir", "Dominick Cadenas", "Marlon Schibi",
+            "Winston Ramsdale", "Tommie Pinley", "Annett Kierce",
+            "Lasandra Batrum", "Elijah Morasca", "Shirleen Roehm", 
+            "Aliza Strause", "Marilu Lankard", "Cary Portor",
+            "Wei Maune", "Aubrey Chahal", "Omega Cloney", 
+            "Jenice Cawthorn", "Timmy Villarrvel", "Emilio Genovesi", 
+            "Elois Bucey", "Tommie Scroggie", "Juliane Yepes",
+            "Randal Deshon", "Juliane Hermenegildo"];
+            
 function lab09parallelArrays() {
     // Your code goes in here.
+    var index;
     var output;
     
-    output = document.getElementById('outputDiv');
+    output = document.getElementById('outputTable');
     
-    output.innerHTML += "The JavaScript file for this page is: "
-            + "\"/unit5/jsFiles/lab09parallelArrays.js\"";
-
+    for(index = 0; index < scores.length; index++){
+        if(scores[index] > 87) {
+            output.innerHTML += "<tr><td>" + names[index] + "</td><td>" + scores[index] + "</td></tr>";
+        }
+    }
 }
--- 2015IntroProg/Documents/unit5/jsFiles/project5Part1.js
+++ 2015IntroProg/Documents/unit5/jsFiles/project5Part1.js
@@ -2,16 +2,65 @@
     This is the JavaScript code for 
     "Find Unique Zip Codes" 
     File: /unit5/project/project5Part1.html.
-	
-	The purpse of this function is to 
-*/
+*/	
+// Initialize constants
+
+// Initialize global variables
+var uniqueZipCodes = [];
+var zipcodeCount = [];
+var index;
+
+// The purpose of this function is to build the unique zip code test
+function isUniqueZipCode(currentZipcode) {
+    // Set flag assuming zipcode is unique and loop through zipcodes
+    var uniqueZipFlag = true;
+    for(index = 0; index < uniqueZipCodes.length; index++) {
+        if(currentZipcode === uniqueZipCodes[index]) {
+            uniqueZipFlag = false;
+            zipcodeCount[index]++;
+            }
+        }
+    // If it is a unique zipcode push to the array and initialize the count at index
+    if(uniqueZipFlag === true) {
+        uniqueZipCodes.push(currentZipcode);
+        zipcodeCount.push(1);
+    }
+}
+    
+//	The purpse of this function is to access and collect information about zipcodeStudyRecords
+function zipCodeStudyRecords() {
+     
+    // Variable Declarations
+    var zipCode;
+    var records;
+ 
+    // Open the Zip Code Study Records and make them 
+    // available to the script
+    records = openZipCodeStudyRecordSet();
+ 
+    while (records.readNextRecord()) {
+        zipCode = records.getSampleZipCode();
+        isUniqueZipCode(zipCode);
+    }
+}
+
+// The purpose of this function is to build the results table
+function createOutputList () {
+    // Store access to div element
+    var output = document.getElementById('outputTable');
+    
+    // Build output table
+    output.innerHTML += "<tr><th>Unique Zipcode</th><th>Count</th></tr>";
+    for (index = 0; index < uniqueZipCodes.length; index++) {
+        output.innerHTML += "<tr><td>" + uniqueZipCodes[index] + "</td><td>"
+                + zipcodeCount[index] + "</td></tr>"; 
+    }
+}
+
 function project5Part1() {
-    // Your code goes in here.
-    var output;
-    
-    output = document.getElementById('outputDiv');
-    
-    output.innerHTML = "The JavaScript file for this page is: "
-            + "\"/unit5/jsFiles/project5Part1.js\"";
+    // invoke functions to access records, test unique zipcode(within records function)
+    // and output the resultant table
+    zipCodeStudyRecords();
+    createOutputList();
    
 }
